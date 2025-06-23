da961bbde78ec4b513a91625f53d81fac1b4d31e
Marco Maggi <mrc.mgg@gmail.com>
2025-06-23 07:37:13 +0200

    devel of tests

--- tests/version.c
+++ tests/version.c
@@ -7,24 +7,22 @@
 
 	Test file for version functions.
 
-  Copyright (C) 2024 Marco Maggi <mrc.mgg@gmail.com>
+  Copyright (C) 2024, 2025 Marco Maggi <mrc.mgg@gmail.com>
 
   See the COPYING file.
 */
 
 #include <mmux-cc-libc.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int
 main (int argc MMUX_CC_LIBC_UNUSED, char const *const argv[] MMUX_CC_LIBC_UNUSED)
 {
-  printf("version number string: %s\n", mmux_cc_libc_version_string());
-  printf("libtool version number: %d:%d:%d\n",
-	 mmux_cc_libc_version_interface_current(),
-	 mmux_cc_libc_version_interface_revision(),
-	 mmux_cc_libc_version_interface_age());
-  exit(EXIT_SUCCESS);
+  mmux_libc_dprintfou("version number string: %s\n", mmux_cc_libc_version_string());
+  mmux_libc_dprintfou("libtool version number: %d:%d:%d\n",
+		      mmux_cc_libc_version_interface_current(),
+		      mmux_cc_libc_version_interface_revision(),
+		      mmux_cc_libc_version_interface_age());
+  mmux_libc_exit(MMUX_LIBC_EXIT_SUCCESS);
 }
 
 /* end of file */
