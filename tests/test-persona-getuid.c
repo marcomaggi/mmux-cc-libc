24d187f57c130c9dda4ec3384da1bdbcc549f0bc
Marco Maggi <mrc.mgg@gmail.com>
2025-07-18 15:47:26 +0200

    persona test

---
+++ tests/test-persona-getuid.c
@@ -0,0 +1,54 @@
+/*
+  Part of: MMUX CC Libc
+  Contents: test for version functions
+  Date: Dec  8, 2024
+
+  Abstract
+
+	Test file for version functions.
+
+  Copyright (C) 2024, 2025 Marco Maggi <mrc.mgg@gmail.com>
+
+  See the COPYING file.
+*/
+
+#include <mmux-cc-libc.h>
+#include <test-common.h>
+
+int
+main (int argc MMUX_CC_LIBC_UNUSED, char const *const argv[] MMUX_CC_LIBC_UNUSED)
+{
+  /* Initialisation. */
+  {
+    mmux_cc_libc_init();
+    PROGNAME = "test-persona-getuid";
+  }
+
+  /* Do it. */
+  {
+    mmux_libc_uid_t	uid;
+
+    if (mmux_libc_getuid(&uid)) {
+      handle_error();
+    }
+
+    {
+      mmux_libc_fd_t	fd;
+
+      mmux_libc_stder(&fd);
+      if (mmux_libc_dprintfer("the UID is: ")) {
+	handle_error();
+      }
+      if (mmux_libc_dprintf_libc_uid(fd, uid)) {
+	handle_error();
+      }
+      if (mmux_libc_dprintfer_newline()) {
+	handle_error();
+      }
+    }
+  }
+
+  mmux_libc_exit_success();
+}
+
+/* end of file */
